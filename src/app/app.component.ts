a3880cbda39b46c9f7df8599109ef5833f55f7ef
pravinpatidar <patidarpravin666@gmail.com>
2023-04-22 17:41:25 +0530

    Implement async pip to show current time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ci-cd azure ci-cid implementation is running';
+  // dateTime = new Date();
+  dateTime = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
 }
